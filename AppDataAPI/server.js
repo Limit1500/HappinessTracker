5ff8f4bbe13eba86e825214115e6b1f2d19c81e9
Limit1500 <david.tamas999@gmail.com>
2024-09-10 20:53:08 +0300

    learning scope changes

--- AppDataAPI/server.js
+++ AppDataAPI/server.js
@@ -1,1 +1,12 @@
-console.log("hi");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("Server has recived your reqest!");
+})
+
+app.listen(PORT, () => {
+    console.log("serverul este on...");
+})
