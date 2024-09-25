e7a4e2b14909163162036adc828ed5b837f92f6e
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-09-25 16:06:15 +0300

    .

--- PawPedia/src/app/pages/breedspage/breedspage.component.ts
+++ PawPedia/src/app/pages/breedspage/breedspage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BreedContainerComponent } from '../../components/breed-container/breed-container.component';
+import { DogsFactComponent } from '../../components/dogs-fact/dogs-fact.component';
 
 @Component({
   selector: 'app-breedspage',
   standalone: true,
-  imports: [BreedContainerComponent],
+  imports: [BreedContainerComponent, DogsFactComponent],
   templateUrl: './breedspage.component.html',
   styleUrl: './breedspage.component.css',
 })
