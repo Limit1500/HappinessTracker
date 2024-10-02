d477702da8e34ee80161c08ff0bd1c9354784985
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-10-02 23:18:27 +0300

    added facts page request

--- PawPedia/src/app/components/dogs-fact/dogs-fact.component.ts
+++ PawPedia/src/app/components/dogs-fact/dogs-fact.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dogs-fact',
   standalone: true,
   imports: [],
   templateUrl: './dogs-fact.component.html',
-  styleUrl: './dogs-fact.component.css'
+  styleUrl: './dogs-fact.component.css',
 })
 export class DogsFactComponent {
-
+  @Input() text: string = '';
 }
--- PawPedia/src/app/pages/factspage/factspage.component.ts
+++ PawPedia/src/app/pages/factspage/factspage.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { BreedsService } from '../../services/breeds.service';
+import { FactsService } from '../../services/facts.service';
 import { DogsFactComponent } from './../../components/dogs-fact/dogs-fact.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-factspage',
   standalone: true,
-  imports: [DogsFactComponent],
+  imports: [DogsFactComponent, CommonModule],
   templateUrl: './factspage.component.html',
   styleUrl: './factspage.component.css',
 })
-export class FactspageComponent {}
+export class FactspageComponent {
+  facts: { text: string }[] = [];
+
+  async pushFacts() {
+    this.facts = await this.factsService.pushFacts(this.facts);
+  }
+
+  ngOnInit() {
+    this.pushFacts();
+  }
+
+  constructor(private factsService: FactsService) {}
+}
