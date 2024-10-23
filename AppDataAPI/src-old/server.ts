d2b434f8c8571ef2d4298f3ec68087f978c9c883
Limit1500 <david.tamas999@gmail.com>
2024-10-23 20:09:01 +0300

    .

---
+++ AppDataAPI/src-old/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { router } from "./routes.js";
+import cors from "cors";
+import { checkDatabaseConnection } from "./userQueries.js";
+
+export const PORT = 4000;
+export const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use("/", router);
+
+server.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:${PORT}`);
+  checkDatabaseConnection();
+});
