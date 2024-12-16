9824d46a33023b05469066b3138b04a5cd9d0306
Limit1500 <david.tamas999@gmail.com>
2024-12-16 07:50:39 +0200

    ,

--- AppDataAPI/src/tests/endpointTests/usersEndpoints.ts
+++ AppDataAPI/src/tests/endpointTests/usersEndpoints.ts
@@ -1,38 +1,37 @@
-import { describe, it } from "mocha";
+import { describe, it } from "node:test";
 import { expect } from "chai";
 import { app } from "../../app.js";
 import request from "supertest";
 import testPayloads from "../helpers/testPayloads.js";
+import userUtils from "../../utils/userUtils.js";
 import { beforeEach } from "node:test";
 import testQueries from "../helpers/testQueries.js";
-import { pool } from "../../config/database.js";
-import userUtils from "../../utils/userUtils.js";
+
+beforeEach(async () => {
+  await testQueries.deleteTestUsers();
+});
 
 describe("Users requests", () => {
   describe("SignIn", () => {
     it("Should return a 200 status code, user is signed in", async () => {
-      await testQueries.deleteTestUsers();
-
       const result = await request(app)
-        .post("/users/signIn")
+        .post("/signIn")
         .send(testPayloads.ValidPayloadNo1);
 
       expect(result.statusCode).equal(200);
     });
     it("Should return a 400 status code, usename or email already used", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .post("/users/signIn")
+        .post("/signIn")
         .send(testPayloads.ValidPayloadNo1);
 
       expect(result.statusCode).equal(400);
     });
     it("Should return a 400 status code, invalid data", async () => {
-      await testQueries.deleteTestUsers();
       const result = await request(app)
-        .post("/users/signIn")
+        .post("/signIn")
         .send(testPayloads.InvalidPayload);
 
       expect(result.statusCode).equal(400);
@@ -41,29 +40,26 @@ describe("Users requests", () => {
 
   describe("LogIn", () => {
     it("Should return a 200 status code, user is logged in", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .post("/users/logIn")
+        .post("/logIn")
         .send(testPayloads.ValidPayloadNo1);
 
       expect(result.statusCode).equal(200);
     });
     it("Should return a 400 status code, invalid data", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .post("/users/logIn")
+        .post("/logIn")
         .send(testPayloads.InvalidPayload);
 
       expect(result.statusCode).equal(400);
     });
     it("Should return a 400 status code, user not found", async () => {
-      await testQueries.deleteTestUsers();
       const result = await request(app)
-        .post("/users/logIn")
+        .post("/logIn")
         .send(testPayloads.ValidPayloadNo1);
 
       expect(result.statusCode).equal(400);
@@ -72,23 +68,18 @@ describe("Users requests", () => {
 
   describe("Delete user", () => {
     it("Should return a 200 status code, user deleted", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .get("/users/deleteUser")
-        .set(
-          "Cookie",
-          `token=${userUtils.createToken(Number(process.env.TEST_ID))}`
-        );
+        .get("/deleteUser")
+        .set("Cookie", userUtils.createToken(Number(process.env.TEST_ID)));
 
       expect(result.statusCode).equal(200);
     });
     it("Should return a 400 status code, token missing", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
-      const result = await request(app).get("/users/deleteUser");
+      const result = await request(app).get("/deleteUser");
 
       expect(result.statusCode).equal(400);
     });
@@ -96,38 +87,29 @@ describe("Users requests", () => {
 
   describe("Edit user data", () => {
     it("Should return a 200 status code, user data edited", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .post("/users/editUserData")
+        .post("/editUserData")
         .send(testPayloads.ValidPayloadNo2)
-        .set(
-          "Cookie",
-          `token=${userUtils.createToken(Number(process.env.TEST_ID))}`
-        );
+        .set("Cookie", userUtils.createToken(Number(process.env.TEST_ID)));
 
       expect(result.statusCode).equal(200);
     });
     it("Should return a 400 status code, token missing", async () => {
-      await testQueries.deleteTestUsers();
-      await testQueries.createTestUserNo1();
+      testQueries.createTestUserNo1();
 
       const result = await request(app)
-        .post("/users/editUserData")
+        .post("/editUserData")
         .send(testPayloads.ValidPayloadNo2);
 
       expect(result.statusCode).equal(400);
     });
     it("Should return a 400 status code, invalid credentials", async () => {
-      await testQueries.deleteTestUsers();
       const result = await request(app)
-        .post("/users/editUserData")
+        .post("/editUserData")
         .send(testPayloads.InvalidPayload)
-        .set(
-          "Cookie",
-          `token=${userUtils.createToken(Number(process.env.TEST_ID))}`
-        );
+        .set("Cookie", userUtils.createToken(Number(process.env.TEST_ID)));
 
       expect(result.statusCode).equal(400);
     });
